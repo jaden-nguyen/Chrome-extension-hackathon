634aebb5a0f2e74ce74b9a4a04bae496f156b5b7
Alex Lopez <alopez.56207@gmail.com>
2022-12-14 23:00:31 -0800

    working chrome extension

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-// Figure out how to read input when you highlight text on a page
-   // Test out the right click eventListener on a sentence in html
-    // research the getSelection() window object
-
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector('body');
     const testText = document.querySelector('h3');
     console.log(testText.innerHTML.toString());
+
     testText.style.width = '100px';
     testText.style.height = '30px';
+
     testText.addEventListener('click', function () {
         var text = "";
         if (window.getSelection) {
@@ -17,20 +15,4 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         console.log(text);
     })
-​
-    
-​
-​
-​
-    // https://www.youtube.com/results?search_query=building+a+chrome+extension
-    // https://www.youtube.com/results?search_query=
-        
-    
-    
-​
-    // testText.addEventListener('click', (c) => {
-    //     console.log('clicked!')
-​
-    // });
-​
 });
