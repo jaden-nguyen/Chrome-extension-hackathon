9a372314ce5acb18ae61b3c0d67c153d7c61ae10
Alex Lopez <alopez.56207@gmail.com>
2022-12-14 20:21:15 -0800

    test

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // Figure out how to read input when you highlight text on a page
    // Test out the right click eventListener on a sentence in html
     // research the getSelection() window object
+
+
+
+    
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector('body');
     const testText = document.querySelector('h3');
