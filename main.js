c6d0268d887685467e05d15599d4d3c8d8d18ed2
Jaden Nguyen <jadenthai206@gmail.com>
2022-12-14 19:25:29 -0800

    All commit

--- main.js
+++ main.js
@@ -4,14 +4,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector('body');
     const testText = document.querySelector('h3');
-
+    testText.focus();
+    testText.setSelectionRange(3,3);
     testText.style.width = '100px';
     testText.style.height = '30px';
+    // this.value = testText.value;
+    // let selection = document.getSelection();
+    // let selRange = selection.getRangeAt(0);
+    // let selectedText = selection.toString();
 
-    let selection = document.getSelection();
-    let selRange = selection.getRangeAt(0);
-    let selectedText = selection.toString();
+    
 
+    testText.addEventListener('click', function () {
+        testText.setSelectionRange(0, testText.value.lastIndexOf('.'));
+        console.log(test);
+    })
+
+    // https://www.youtube.com/results?search_query=building+a+chrome+extension
+    // https://www.youtube.com/results?search_query=
         
     
     
